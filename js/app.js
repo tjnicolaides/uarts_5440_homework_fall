4d3faa7a8333f02de4f5f2cbc05da2c4181a3305
TJ Nicolaides <tjnicolaides@gmail.com>
2013-10-23 19:36:37 -0400

    example of finished Homework #2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
-var to_dos = ['Take out the garbage', 'Wash the dishes', 'Grade homework', 'Vacuum and dust', 'Kick back and relax'];
+var to_dos = [{'complete': false },
+{'description' : 'mess up my to do app'},
+{'description': 'Take out the garbage', 'complete': false}, 
+{'description': 'Wash the dishes', 'complete': false}, 
+{'description': 'Grade homework', 'complete': true}, 
+{'description': 'Vacuum and dust', 'complete': false}, 
+{'description': 'Kick back and relax', 'complete': true}];
 
+var count = 1;
 document.write('<ul class="list-group">');
 
 for(var i=0; i < to_dos.length; i++) {
-
-    document.write('<li class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>   ' + to_dos[i] + '</li>'); 
+    
+    try {
+        printTask(to_dos[i]);
+    } catch(error) {
+        console.log("There was an error: " + error);
+    }
     
 }    
 
 document.write('</ul>');
+
+function printTask(task) {
+    
+    if(typeof task.description == 'undefined') {
+        throw 'This task is missing a description!';
+    }
+    
+    if(typeof task.complete == 'undefined') {
+        throw 'This task is missing a complete status!';
+    }
+        
+    if(task.complete) {
+        document.write('<li class="list-group-item success">' + count + '<span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'); 
+    } else {
+        document.write('<li class="list-group-item">' + count + '<span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'); 
+    }
+    
+    count++;
+}
