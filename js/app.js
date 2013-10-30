f93e2d46c6546db95fa9fd6b769ce084bd6eaf7e
TJ Nicolaides <tjnicolaides@gmail.com>
2013-10-30 19:44:37 -0400

    week 3 homework example

--- js/app.js
+++ js/app.js
@@ -1,25 +1,4 @@
-var to_dos = [{'complete': false },
-{'description' : 'mess up my to do app'},
-{'description': 'Take out the garbage', 'complete': false}, 
-{'description': 'Wash the dishes', 'complete': false}, 
-{'description': 'Grade homework', 'complete': true}, 
-{'description': 'Vacuum and dust', 'complete': false}, 
-{'description': 'Kick back and relax', 'complete': true}];
 
-var count = 1;
-document.write('<ul class="list-group">');
-
-for(var i=0; i < to_dos.length; i++) {
-    
-    try {
-        printTask(to_dos[i]);
-    } catch(error) {
-        console.log("There was an error: " + error);
-    }
-    
-}    
-
-document.write('</ul>');
 
 function printTask(task) {
     
@@ -32,10 +11,45 @@ function printTask(task) {
     }
         
     if(task.complete) {
-        document.write('<li class="list-group-item success">' + count + '<span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'); 
+        return '<li class="list-group-item success"><span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'; 
     } else {
-        document.write('<li class="list-group-item">' + count + '<span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'); 
+        return '<li class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>   ' + task.description + '</li>'; 
     }
     
-    count++;
 }
+
+function removeCompleted() {
+    $('.success').remove();
+}
+
+
+$(document).ready(function(){
+    var to_dos = [{'complete': false },
+    {'description' : 'mess up my to do app'},
+    {'description': 'Take out the garbage', 'complete': false}, 
+    {'description': 'Wash the dishes', 'complete': false}, 
+    {'description': 'Grade homework', 'complete': true}, 
+    {'description': 'Vacuum and dust', 'complete': false}, 
+    {'description': 'Kick back and relax', 'complete': true}];
+    
+    var html_container = '<ul class="list-group">';
+    
+    for(var i=0; i < to_dos.length; i++) {
+        
+        try {
+            html_container += printTask(to_dos[i]);
+        } catch(error) {
+            console.log("There was an error: " + error);
+        }
+        
+    }    
+    
+    html_container += '</ul>';
+    
+    console.log(html_container);
+    
+    $('.panel-primary').append(html_container);    
+});
+
+
+
