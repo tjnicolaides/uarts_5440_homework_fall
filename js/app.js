f17949561aedf02be789366175052eebe9043eb9
TJ Nicolaides <tjnicolaides@gmail.com>
2013-11-13 19:27:21 -0500

    another GET/POST example

--- js/app.js
+++ js/app.js
@@ -42,38 +42,69 @@ function addTask(description) {
     // fire new Task();
     
     var newTask = new Task(description);
+    console.log(JSON.stringify(newTask));
+    
+    $.ajax({
+        'url': 'http://api.usergrid.com/tjnicolaides/to_dos/to_dos',
+        'type': 'POST',
+        'data' : JSON.stringify(newTask),
+        'success': function(data) {
+            console.log(data);
+        }
+    });
     
     to_dos.push(newTask); // add the new task to to_dos using .push
     
     var $li = printTask(newTask);
 
     $('ul.nav-list').append($li);  // add the new task to the DOM
+    
 }
 
 var to_dos;
 
 $(document).ready(function(){
-    to_dos = [{'complete': false },
-    {'description' : 'mess up my to do app'},
-    {'description': 'Take out the garbage', 'complete': false}, 
-    {'description': 'Wash the dishes', 'complete': false}, 
-    {'description': 'Grade homework', 'complete': true}, 
-    {'description': 'Vacuum and dust', 'complete': false}, 
-    {'description': 'Kick back and relax', 'complete': true}];
     
-    var $ul = $('<ul/>', {'class' : 'nav nav-list'});
+    $.ajax({
+        'url': 'http://api.usergrid.com/tjnicolaides/to_dos/to_dos',
+        'type': 'GET', 
+        'success': function(data){
+            console.log(data);
+            to_dos = data.entities;
+            /*to_dos = [{'complete': false },
+            {'description' : 'mess up my to do app'},
+            {'description': 'Take out the garbage', 'complete': false}, 
+            {'description': 'Wash the dishes', 'complete': false}, 
+            {'description': 'Grade homework', 'complete': true}, 
+            {'description': 'Vacuum and dust', 'complete': false}, 
+            {'description': 'Kick back and relax', 'complete': true}];*/
+            
+            var $ul = $('<ul/>', {'class' : 'nav nav-list'});
+            
+            for(var i=0; i < to_dos.length; i++) {
+                
+                try {
+                    $ul.append(printTask(to_dos[i]));
+                } catch(error) {
+                    console.log("There was an error: " + error);
+                }
+                
+            }    
+            
+            $('.panel-primary').append($ul);
     
-    for(var i=0; i < to_dos.length; i++) {
-        
-        try {
-            $ul.append(printTask(to_dos[i]));
-        } catch(error) {
-            console.log("There was an error: " + error);
         }
-        
-    }    
+    });
     
-    $('.panel-primary').append($ul);    
+    $('#newTaskForm').on('submit', function(event){
+        event.preventDefault();
+
+        var newTaskName = $(this).find('input').val();
+        
+        addTask(newTaskName);
+    });
+
+        
 
 });
 
